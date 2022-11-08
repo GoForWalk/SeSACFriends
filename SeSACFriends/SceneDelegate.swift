24c61c679d6dcd3b94ddf46b3be5b6a84df7a070
GoForWalk <goforwalk@gmail.com>
2022-11-08 21:54:14 +0900

    2022.11.08 PhoneAuth 동작 확인 code ## commit 주요사항 # TODO: 화면에 키보드 내리기, 로직단 짜기
    # New Files
    
    #modified Files
    info.plist
    SceneDelegate.swift
    
    #Deleted Files

--- SeSACFriends/SceneDelegate.swift
+++ SeSACFriends/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
         
-        let rootViewController = OnboardingViewController()
+        let rootViewController = PhoneAuthViewController()
         let navVC = UINavigationController(rootViewController: rootViewController)
         
         self.window?.rootViewController = rootViewController
